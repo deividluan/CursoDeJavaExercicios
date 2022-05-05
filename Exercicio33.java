96d60c601a116b9afd7aba1143bd65f871cd7fc0
deividluan <104109599+deividluan@users.noreply.github.com>
2022-05-04 21:39:34 -0300

    Add files via upload

---
+++ Exercicio33.java
@@ -0,0 +1,27 @@
+package ExercicioTeste;
+
+import java.util.Scanner;
+
+public class Exercicio33 {
+	public static void main (String[]args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int numero1, numero2 = 0;
+		
+		System.out.print("Digite um valor: ");
+		numero1 = sc.nextInt();
+		
+		System.out.print("Digite outro valor: ");
+		numero2 = sc.nextInt();
+		
+		if (numero1 > numero2) {
+			System.out.print("Primeiro é maior");
+		}
+		else if (numero2 > numero1) {
+			System.out.print("Segundo é maior");
+		}
+		else {
+			System.out.print("Números iguais");
+		}
+	}
+}
