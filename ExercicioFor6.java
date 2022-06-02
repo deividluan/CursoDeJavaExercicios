6a1243d2313c7dd851f96975589bd97b7e1b348a
deividluan <104109599+deividluan@users.noreply.github.com>
2022-06-01 22:16:07 -0300

    Add files via upload

--- ExercicioFor6.java
+++ ExercicioFor6.java
@@ -2,20 +2,12 @@ package ExercicioTeste;
 
 public class ExercicioFor6 {
 	public static void main (String[]args) {
-		int expoente = 0;
-		int base = 3;
 		int cont = 0;
-		int baseIni = 3;
 		
-		for (expoente = 1; expoente < 15; expoente++) {
-			while (cont < expoente) {
-				if (cont != 0) {
-					base *= baseIni;
-				}
-				cont ++;
+		for (cont = 1; cont < 200; cont++) {
+			if (cont % 4 == 0) {
+				System.out.print(cont + ", ");
 			}
-			
-			System.out.println("3 ^ " + expoente + " = " + base);
 		}
 	}
 }
