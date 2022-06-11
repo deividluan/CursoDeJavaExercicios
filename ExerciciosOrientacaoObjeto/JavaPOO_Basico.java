83611119a42a87f05b3cfe50a13baf740ae4a8f7
deividluan <104109599+deividluan@users.noreply.github.com>
2022-06-10 22:35:59 -0300

    Add files via upload

--- ExerciciosOrientacaoObjeto/JavaPOO_Basico.java
+++ ExerciciosOrientacaoObjeto/JavaPOO_Basico.java
@@ -1,5 +1,14 @@
 package ExerciciosOrientacaoObjeto;
 
 public class JavaPOO_Basico {
-
+	public static void main(String[] args) {
+		
+		JavaPOO_Classes GuardaRoupa = new JavaPOO_Classes();
+		
+		GuardaRoupa.coisas = "Roupa, meia, Cabide, Gaveta";
+		GuardaRoupa.Ultilidade = "Abrir e Fechar";
+		GuardaRoupa.Estado = "Fechado";
+		GuardaRoupa.GuardaRoupa();
+		
+	}
 }
--- ExerciciosOrientacaoObjeto/JavaPOO_Classes.java
+++ ExerciciosOrientacaoObjeto/JavaPOO_Classes.java
@@ -1,5 +1,18 @@
 package ExerciciosOrientacaoObjeto;
 
 public class JavaPOO_Classes {
+	
+	String coisas;
+	String Ultilidade;
+	String Estado;
+	
+	void GuardaRoupa() {
+		System.out.println("O Guarda roupa tem: " + this.coisas);
+		System.out.println("eu posso: " + this.Ultilidade + " o armario");
+		System.out.println("Ele esta: " + this.Estado);
+		
+	}
+	public static void status() {
 
+	}
 }
