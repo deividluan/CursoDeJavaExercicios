32ffe0ca238064b3f7d7264c130d9cc285ebeafd
deividluan <104109599+deividluan@users.noreply.github.com>
2022-06-13 17:34:42 -0300

    Add files via upload

---
+++ ExerciciosOrientacaoObjeto/JavaPOO_Basico2.java
@@ -0,0 +1,15 @@
+package ExerciciosOrientacaoObjeto;
+
+public class JavaPOO_Basico2 {
+	public static void main (String[]args) {
+		
+		JavaPOO_Classes Classes = new JavaPOO_Classes();
+		
+		Classes.Objeto = "Estojo";
+		Classes.coisas = "Caneta, Lapis, Borracha, Tesoura";
+		Classes.Ultilidade = "Abrir e fechar";
+		Classes.Estado = "Fechado";
+		Classes.Classes();
+		
+	}
+}
